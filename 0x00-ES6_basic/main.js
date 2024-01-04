146334cb49c627c8f702e4a6782e282b434a66c9
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-04 06:41:19 +0300

    spread operator is effectivly used

---
+++ 0x00-ES6_basic/main.js
@@ -0,0 +1,7 @@
+import { taskFirst, taskNext } from "./0-constants.js";
+import returnHowManyArguments from "./4-rest-parameter.js";
+import concatArrays from "./5-spread-operator.js";
+
+console.log(`${taskFirst()} ${taskNext()}}`);
+console.log(`${returnHowManyArguments("sd","dkfjsldjf",4,45)}`);
+console.log(`${concatArrays(['a','b','c'],['d','e','f'],'Hello')}`);
