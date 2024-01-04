749941afd5da9cf3f4574e928f4743fc8afe943c
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-04 10:02:45 +0300

    js object notation and getting attributes

--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -4,10 +4,17 @@ import concatArrays from "./5-spread-operator.js";
 import getSanFranciscoDescription from "./6-string-interpolation.js";
 import getBudgetObject from "./7-getBudgetObject.js";
 import getBudgetForCurrentYear from "./8-getBudgetCurrentYear.js";
+import getFullBudgetObject from "./9-getFullBudget.js";
 
 console.log(`${taskFirst()} ${taskNext()}}`);
 console.log(`${returnHowManyArguments("sd","dkfjsldjf",4,45)}`);
 console.log(`${concatArrays(['a','b','c'],['d','e','f'],'Hello')}`);
 //console.log(`${getSanFranciscoDescription()}`);
 console.log(getBudgetObject(400, 700, 900));
-console.log(getBudgetForCurrentYear(2100, 5200, 1090));
+console.log(getFullBudgetObject(2100, 5200, 1090));
+console.log('----------------------------------------------');
+
+const fullBudget = getFullBudgetObject(20, 50, 10);
+console.log(fullBudget.gdp);
+console.log(fullBudget.getIncomeInDollars(fullBudget.income));
+console.log(fullBudget.getIncomeInEuros(fullBudget.income));
