94f56fbe3a82762ffdf430918adacae1504f9628
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-04 09:47:37 +0300

    shorthand expression for object

--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -2,8 +2,10 @@ import { taskFirst, taskNext } from "./0-constants.js";
 import returnHowManyArguments from "./4-rest-parameter.js";
 import concatArrays from "./5-spread-operator.js";
 import getSanFranciscoDescription from "./6-string-interpolation.js";
+import getBudgetObject from "./7-getBudgetObject.js";
 
 console.log(`${taskFirst()} ${taskNext()}}`);
 console.log(`${returnHowManyArguments("sd","dkfjsldjf",4,45)}`);
 console.log(`${concatArrays(['a','b','c'],['d','e','f'],'Hello')}`);
-console.log(`${getSanFranciscoDescription()}`);
+//console.log(`${getSanFranciscoDescription()}`);
+console.log(getBudgetObject(400, 700, 900));
