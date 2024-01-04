d45e6f8f29375a05ebf8a206413c18ca6f2174bf
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-04 10:24:21 +0300

    create an employee object

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+    let newArray=[];
+    for (const idx of array) {
+        newArray.push(appendString+idx);
+    }
+  
+    return newArray;
+  }
+  
--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -5,16 +5,21 @@ import getSanFranciscoDescription from "./6-string-interpolation.js";
 import getBudgetObject from "./7-getBudgetObject.js";
 import getBudgetForCurrentYear from "./8-getBudgetCurrentYear.js";
 import getFullBudgetObject from "./9-getFullBudget.js";
+import appendToEachArrayValue from "./10-loops.js";
+import createEmployeesObject from "./11-createEmployeesObject.js";
 
 console.log(`${taskFirst()} ${taskNext()}}`);
 console.log(`${returnHowManyArguments("sd","dkfjsldjf",4,45)}`);
 console.log(`${concatArrays(['a','b','c'],['d','e','f'],'Hello')}`);
 //console.log(`${getSanFranciscoDescription()}`);
 console.log(getBudgetObject(400, 700, 900));
-console.log(getFullBudgetObject(2100, 5200, 1090));
 console.log('----------------------------------------------');
 
 const fullBudget = getFullBudgetObject(20, 50, 10);
 console.log(fullBudget.gdp);
 console.log(fullBudget.getIncomeInDollars(fullBudget.income));
 console.log(fullBudget.getIncomeInEuros(fullBudget.income));
+console.log('----------------------------------------------');
+console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
+console.log('----------------------------------------------');
+console.log(createEmployeesObject("Software", [ "Bob", "Sylvie" ]));
