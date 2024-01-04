3fcfbf8cc0364d93ad33005c2d7836826de41611
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-04 09:30:54 +0300

    string interpolation is used

--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -1,7 +1,9 @@
 import { taskFirst, taskNext } from "./0-constants.js";
 import returnHowManyArguments from "./4-rest-parameter.js";
 import concatArrays from "./5-spread-operator.js";
+import getSanFranciscoDescription from "./6-string-interpolation.js";
 
 console.log(`${taskFirst()} ${taskNext()}}`);
 console.log(`${returnHowManyArguments("sd","dkfjsldjf",4,45)}`);
 console.log(`${concatArrays(['a','b','c'],['d','e','f'],'Hello')}`);
+console.log(`${getSanFranciscoDescription()}`);
