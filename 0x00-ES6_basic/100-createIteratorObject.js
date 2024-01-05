f6976e4ab94432415829fbc46735c5acea522fed
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-05 06:14:47 +0300

    creating iterator object is done

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,15 @@
+export default function createIteratorObject(report) {
+    // returns a list of employees from each object.
+    let employeeList=[];
+    for(const dep in report.allEmployees){
+        // dep is the a property and here we are checking
+        // if that property exists.
+       if(report.allEmployees.hasOwnProperty(dep)){
+        const employees = report.allEmployees[dep];
+        for(const employee of employees){
+           employeeList.push(employee);
+        }
+       }
+    }
+    return employeeList;
+}
--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -1,34 +1,20 @@
-import { taskFirst, taskNext } from "./0-constants.js";
-import returnHowManyArguments from "./4-rest-parameter.js";
-import concatArrays from "./5-spread-operator.js";
-import getBudgetObject from "./7-getBudgetObject.js";
-import getFullBudgetObject from "./9-getFullBudget.js";
-import appendToEachArrayValue from "./10-loops.js";
 import createEmployeesObject from "./11-createEmployeesObject.js";
 import createReportObject from "./12-createReportObject.js";
+import createIteratorObject from "./100-createIteratorObject.js";
 
-console.log(`${taskFirst()} ${taskNext()}}`);
-console.log(`${returnHowManyArguments("sd","dkfjsldjf",4,45)}`);
-console.log(`${concatArrays(['a','b','c'],['d','e','f'],'Hello')}`);
-//console.log(`${getSanFranciscoDescription()}`);
-console.log(getBudgetObject(400, 700, 900));
-console.log('----------------------------------------------');
-
-const fullBudget = getFullBudgetObject(20, 50, 10);
-console.log(fullBudget.gdp);
-console.log(fullBudget.getIncomeInDollars(fullBudget.income));
-console.log(fullBudget.getIncomeInEuros(fullBudget.income));
-console.log('----------------------------------------------');
-console.log(appendToEachArrayValue(['appended', 'fixed', 'displayed'], 'correctly-'));
-console.log('----------------------------------------------');
-console.log(createEmployeesObject("Software", [ "Bob", "Sylvie" ]));
 console.log('----------------------------------------------');
 const employees = {
     ...createEmployeesObject('engineering', ['Bob', 'Jane']),
     ...createEmployeesObject('marketing', ['Sylvie'])
 };      
 
-const report = createReportObject(employees);
-console.log(report.allEmployees);
+const report1 = createReportObject(employees);
+console.log(report1.allEmployees);
 console.log('----------------------------------------------');
-console.log(report.getNumberOfDepartments(report.allEmployees));
+const report = createReportObject(employees);
+
+const reportWithIterator = createIteratorObject(report);
+
+for (const item of reportWithIterator) {
+    console.log(item);
+}
