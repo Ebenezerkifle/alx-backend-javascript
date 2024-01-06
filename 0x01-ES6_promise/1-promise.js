a71fe0e99fa272458a34ccf932d8f80374c566b2
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-06 07:25:09 +0300

    some formats are corrected

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,17 +1,17 @@
 export default function getFullResponseFromAPI(sucess){
-    return new Promise(
-        (resolved, reject)=>{
-            if(sucess){
-                resolved(
-                    {
-                        "status":200,
-                        "body":"Success"
-                    }
-                )
-            }
-            else{
-                reject(Error("The fake Api is not working currently"));
-            }
-        }
-    );
+  return new Promise(
+    (resolved, reject)=>{
+      if(sucess){
+        resolved(
+          {
+            status:200,
+            body:"Success"
+          }
+        )
+      }
+      else{
+        reject(Error("The fake Api is not working currently"));
+      }
+    }
+  );
 }
---
+++ formats.js
@@ -0,0 +1,13 @@
+// space before opening brace. for all
+//------------------ONE-----------------------
+// export default function example(a)/*here*/{}
+//------------------TWO-----------------------
+// indentation should be only 2. for all
+//------------------THREE--------------------
+// Space before and afrer arrow
+// -------------------FOUR-------------------
+// expected spacing after if keyword.
+// ------------------FIVE-------------------
+// strings should have single quote.
+//-------------------SIX-----------------
+// new line at the end of the file needed
