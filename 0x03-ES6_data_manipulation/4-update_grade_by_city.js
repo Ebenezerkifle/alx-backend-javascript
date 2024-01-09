35e1bbfa8ece785b96116cad423b4132cba6ba1d
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-09 09:59:37 +0300

    updating an object is done

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,7 @@
 export default function updateStudentGradeByCity(array, city, newGrades) {
-  const filtered = array.filter((studnets) => studnets.location == city);
-  const students = filtered.map((student) => {
+  return array.filter((studnets) => studnets.location == city).map((student) => {
     const myGrade = newGrades.filter((grade) => grade.studentId == student.id)[0];
     student['grade'] = (myGrade != undefined && myGrade.grade != undefined) ? myGrade.grade : 'N/A';
     return student;
   });
-  return students;
 }
