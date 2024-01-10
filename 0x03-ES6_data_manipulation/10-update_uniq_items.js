b44f95dceb7af2ce1d2bcb713e762bd2a6b38862
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-10 04:46:28 +0300

    updating a value on a map

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,5 @@
 export default function updateUniqueItems(map) {
-  for (const [key, value] of map){
-    if (value == 1) {
-        map.set(key, 100);
-    }
-  }
+  if (!(map instanceof Map)) throw Error('Cannot process');
+  for (const [key, value] of map) if (value == 1) map.set(key, 100);
   return map;
 }
