eacd99523af6bfdcab5e13d6a9ad663f1931f610
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-10 05:00:29 +0300

    endpoint load is hight

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,10 +1,10 @@
 export const weakMap = new WeakMap();
 
 export default function queryAPI(endPoint) {
-  if (weakMap.has(endPoint)){
+  if (weakMap.has(endPoint)) {
     // if aready has
     const nQuery = weakMap.get(endPoint);
-    if (nQuery >= 4) throw(Error("Endpoint load is high"));
+    if (nQuery >= 4) throw Error("Endpoint load is high");
     weakMap.set(endPoint, nQuery + 1);
   }
   else{ weakMap.set(endPoint, 1); }
