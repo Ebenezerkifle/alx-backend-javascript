573898af120f00df44efdb2b3c9d7adffdb7d7f6
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-10 05:11:15 +0300

    changes made in main file

--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -4,8 +4,6 @@ import createIteratorObject from "./100-createIteratorObject.js";
 import iterateThroughObject from "./101-iterateThroughObject.js";
 import getListStudentIds from "../0x03-ES6_data_manipulation/1-get_list_student_ids.js";
 import getListStudents from "../0x03-ES6_data_manipulation/0-get_list_students.js";
-import getStudentsByLocation from "../0x03-ES6_data_manipulation/2-get_students_by_loc.js";
-import getStudentIdsSum from "../0x03-ES6_data_manipulation/3-get_ids_sum.js";
 import updateStudentGradeByCity from "../0x03-ES6_data_manipulation/4-update_grade_by_city.js";
 import setFromArray from "../0x03-ES6_data_manipulation/6-set.js";
 import hasValuesFromArray from "../0x03-ES6_data_manipulation/7-has_array_values.js";
@@ -45,14 +43,6 @@ console.log(getListStudentIds(getListStudents()));
 console.log(getListStudentIds("Abeni"));
 console.log('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%50');
 
-// const students = getListStudents();
-
-// console.log(getStudentsByLocation(students, 'San Francisco'));
-
-// const students = getListStudents();
-// const value = getStudentIdsSum(students);
-
-// console.log(value);
 console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }, { studentId: 1, grade: 86 }]));
 
 console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }]));
