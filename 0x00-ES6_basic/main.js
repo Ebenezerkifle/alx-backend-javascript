8ff1b2fca8f5b07a27d0ecbf9e8df0789b7af4e3
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-01-10 04:43:06 +0300

    updating a value on a map

--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -10,6 +10,8 @@ import updateStudentGradeByCity from "../0x03-ES6_data_manipulation/4-update_gra
 import setFromArray from "../0x03-ES6_data_manipulation/6-set.js";
 import hasValuesFromArray from "../0x03-ES6_data_manipulation/7-has_array_values.js";
 import cleanSet from "../0x03-ES6_data_manipulation/8-clean_set.js";
+import groceriesList from "../0x03-ES6_data_manipulation/9-groceries_list.js";
+import updateUniqueItems from "../0x03-ES6_data_manipulation/10-update_uniq_items.js";
 
 console.log('----------------------------------------------');
 const employees = {
@@ -67,3 +69,10 @@ console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 10]));
 console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&7');
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
+
+console.log('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5');
+const map = groceriesList();
+console.log(map);
+
+updateUniqueItems(map)
+console.log(map);
